e7391e67379306bcf4046ed83496e9c6fbb15fb5
Neil Livingstone <neil@livingstone.co>
2020-05-17 16:25:37 -0400

    Remove sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-sudo apt update
-sudo apt install git
+apt update
+apt install git
 
 curl -O https://dl.google.com/go/go1.14.3.linux-amd64.tar.gz
 tar xvf go1.14.3.linux-amd64.tar.gz
-sudo chown -R root:root ./go
-sudo mv go /usr/local
+chown -R root:root ./go
+mv go /usr/local
 export GOPATH=$HOME/work
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
