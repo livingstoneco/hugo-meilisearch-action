f03bfac912bca918e60b88931d7b2e81016b5b1f
Neil Livingstone <neil@livingstone.co>
2020-05-17 18:49:02 -0400

    wrap in async

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const MeiliSearch = require('meilisearch');
+var fs = require('fs');
 
-try {
-    const key = core.getInput('key')
-    const host = core.getInput('host')
-    const indexName = core.getInput('indexName')
-
-    // Connect to MeiliSearch Instance
-    console.log(`Connecting to ${host}:${indexName}!`)
-    const client = new MeiliSearch({
-        host: host,
-        apiKey: key,
-    })
-
-    // Does index already exist, if so delete it
-    let index = await client.listIndexes();
-	index.forEach(function(item) {
-		if(item.name == indexName) {
-			let posts = client.getIndex(indexName)
-			posts.deleteIndex()
-		}
-	})
-
-    const index = await client.createIndex({ uid: indexName, primaryKey: 'id' })
-    const searchIndex = await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
-    const response = await index.addDocuments(searchIndex);
-    console.log(response) // => { "updateId": 0 }
-
-} catch (error) {
-  core.setFailed(error.message);
-}
+;(async () => {
+    try {
+        const key = core.getInput('key')
+        const host = core.getInput('host')
+        const indexName = core.getInput('indexName')
+    
+        // Connect to MeiliSearch Instance
+        console.log(`Connecting to ${host}:${indexName}!`)
+        const client = new MeiliSearch({
+            host: host,
+            apiKey: key,
+        })
+    
+        // Does index already exist, if so delete it
+        let index = await client.listIndexes();
+        index.forEach(function(item) {
+            if(item.name == indexName) {
+                let posts = client.getIndex(indexName)
+                posts.deleteIndex()
+            }
+        })
+    
+        const index = await client.createIndex({ uid: indexName, primaryKey: 'id' })
+        const searchIndex = await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
+        const response = await index.addDocuments(searchIndex);
+        console.log(response) // => { "updateId": 0 }
+    
+    } catch (error) {
+      core.setFailed(error.message);
+    }    
+});
