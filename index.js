eb3e9bc34938dbaddac5c7eeae7f53eccce14f36
Neil Livingstone <neil@livingstone.co>
2020-05-17 16:01:02 -0400

    List files in current directory

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
   console.log('hello world');
+  fs.readdir('./', (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
