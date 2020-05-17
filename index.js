fc589b84915a9e965685c1de76a7bb711287ec9f
Neil Livingstone <neil@livingstone.co>
2020-05-17 18:44:06 -0400

    Add code to push index to instance

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const github = require('@actions/github');
 const MeiliSearch = require('meilisearch');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const key = core.getInput('key');
-  const host = core.getInput('host');
-  const indexName = core.getInput('indexName'); 
-  console.log(`Connecting to ${host}:${indexName}!`);
+    const key = core.getInput('key')
+    const host = core.getInput('host')
+    const indexName = core.getInput('indexName')
+
+    // Connect to MeiliSearch Instance
+    console.log(`Connecting to ${host}:${indexName}!`)
+    const client = new MeiliSearch({
+        host: host,
+        apiKey: key,
+    })
+
+    // Does index already exist, if so delete it
+    let index = await client.listIndexes();
+	index.forEach(function(item) {
+		if(item.name == 'posts') {
+			let posts = client.getIndex('posts')
+			posts.deleteIndex()
+		}
+	})
+
+    const index = await client.createIndex({ uid: indexName, primaryKey: 'id' })
+    const searchIndex = await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
+    const response = await index.addDocuments(searchIndex);
+    console.log(response) // => { "updateId": 0 }
 
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
