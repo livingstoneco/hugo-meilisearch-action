5b6a260030239b3a455abf6cbbb726747186d190
Neil Livingstone <neil@livingstone.co>
2020-05-17 19:25:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ var fs = require('fs');
 // ;(async () => {
     const ls = exec.exec('ls -la docs')
     console.log(ls)
-    // try {
-    //     const key = core.getInput('key')
-    //     const host = core.getInput('host')
-    //     const indexName = core.getInput('indexName')
+    try {
+        const key = core.getInput('key')
+        const host = core.getInput('host')
+        const indexName = core.getInput('indexName')
     
-    //     // Connect to MeiliSearch Instance
-    //     console.log(`Connecting to ${host}:${indexName}!`)
-    //     const client = new MeiliSearch({
-    //         host: host,
-    //         apiKey: key,
-    //     })
+        // Connect to MeiliSearch Instance
+        console.log(`Connecting to ${host}:${indexName}!`)
+        const client = new MeiliSearch({
+            host: host,
+            apiKey: key,
+        })
     
-    //     // Does index already exist, if so delete it
-    //     let index = await client.listIndexes();
-    //     index.forEach(function(item) {
-    //         if(item.name == indexName) {
-    //             let posts = client.getIndex(indexName)
-    //             posts.deleteIndex()
-    //         }
-    //     })
+        // Does index already exist, if so delete it
+        let index = client.listIndexes();
+        index.forEach(function(item) {
+            if(item.name == indexName) {
+                let posts = client.getIndex(indexName)
+                posts.deleteIndex()
+            }
+        })
     
-    //     index = await client.createIndex({ uid: indexName, primaryKey: 'id' })
-    //     const searchIndex = await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
-    //     console.log(searchIndex)
-    //     const response = await index.addDocuments(searchIndex);
-    //     console.log(response)
+        index = client.createIndex({ uid: indexName, primaryKey: 'id' })
+        const searchIndex = JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
+        console.log(searchIndex)
+        const response = index.addDocuments(searchIndex);
+        console.log(response)
     
-    // } catch (error) {
-    //   core.setFailed(error.message);
-    // }    
+    } catch (error) {
+      core.setFailed(error.message);
+    }    
 // });
