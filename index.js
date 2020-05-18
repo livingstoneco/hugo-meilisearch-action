a2aabd27d0686819cfd2a250d585b3930838f96a
Neil Livingstone <neil@livingstone.co>
2020-05-17 20:05:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
 const MeiliSearch = require('meilisearch');
 var fs = require('fs');
 
+const config = {
+    host: core.getInput('host'),
+    apiKey: core.getInput('key'),
+}
+var client = new MeiliSearch(config)
+
+const index = {
+  uid: core.getInput('indexName'),
+}
+
+const dataset = (async () => {
+    return await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
+})()
+
+console.log(dataset);
+
 ;(async () => {
-    try {
-        const key = core.getInput('key')
-        const host = core.getInput('host')
-        const indexName = core.getInput('indexName')
-    
-        // Connect to MeiliSearch Instance
-        console.log(`Connecting to ${host}:${indexName}!`)
-        const client = new MeiliSearch({
-            host: host,
-            apiKey: key,
-        })
-    
-        // Does index already exist, if so delete it
-        let index = await client.listIndexes();
-        index.forEach(function(item) {
-            if(item.name == indexName) {
-                let posts = client.getIndex(indexName)
-                posts.deleteIndex()
-            }
-        })
-    
-        index = await client.createIndex({ uid: indexName, primaryKey: 'id' })
-        const searchIndex = await JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
-        const response = await index.addDocuments(searchIndex);
-    
-    } catch (error) {
-      core.setFailed(error.message);
-    }    
-});
+    await client.createIndex(index.uid)
+    await client.getIndex(index.uid).addDocuments(dataset)
+})()
