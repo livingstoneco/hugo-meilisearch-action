12e980dfc2eb186787ab3f18b45c62aac5ca423c
Neil Livingstone <neil@livingstone.co>
2020-05-18 16:09:53 -0400

    Add comments

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const core = require('@actions/core');
 const MeiliSearch = require('meilisearch');
 var fs = require('fs');
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const config = {
     host: core.getInput('host'),
     apiKey: core.getInput('key'),
 }
-var client = new MeiliSearch(config)
 
 const index = {
-  uid: core.getInput('indexName'),
+	uid: core.getInput('indexName'),
 }
+
+// Create MeiliSearch client
+var client = new MeiliSearch(config)
 
 const indexes = client.listIndexes()
 
+// Delete index if already exists
 indexes.then(function(data) {
 	data.forEach(function(item) {
 		if(item.uid === index.uid) {
@@ -24,9 +31,7 @@ indexes.then(function(data) {
 	})
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+
 
 const dataset = JSON.parse(fs.readFileSync('./docs/searchindex.json', 'utf8'));
 
